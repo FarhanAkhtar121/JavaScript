6376587cbe6343c17ac4969dc09fd251c0a39df5
FARHAN AKHTAR <46897669+FarhanAkhtar121@users.noreply.github.com>
2024-02-19 07:33:04 +0000

    merging objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,61 @@
+const tinderUser = {}
+
+tinderUser.id = "12abc"
+tinderUser.name = "sammy"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+const regulaUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname: "Farhan",
+            latname: "Akhtar"
+        }
+    }
+}
+
+console.log(regulaUser);
+
+// Merging one object with another one
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+
+const obj3 = Object.assign(obj1, obj2)  // return obj = Object.assign(target obj, source obj)
+console.log(obj3);
+
+const obj4 = Object.assign({},obj1, obj2)   
+console.log(obj4);
+
+const obj5 = {...obj1 , ...obj2}    // using spread object to merge tqo objects
+console.log(obj5);
+
+// /////////////////////////////////// Array of objects /////////////////////////////////////
+
+const users = [
+    {
+        id: 1,
+        email: "new@gmail.com"
+    },
+    {
+        id: 2,
+        email: "new2@gmail.com"
+    },
+    {
+        id: 3,
+        email: "new3@gmail.com"
+    }
+]
+
+users[1].email
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
